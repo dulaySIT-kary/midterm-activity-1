97f810bdfcc64a4b40dafd205888129e66567ce3
dulay_k <dulay_k@ca01.ub.lan>
2026-03-09 12:10:45 +0800

    Home page

---
+++ midterm-activity-1/src/pages/homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <h1>Welcome to Our Website</h1>
+      <p>This is the homepage. Feel free to explore!</p>
+    </div>
+  );
+};
+
+export default Homepage;
